f9c5d410d02f27b3c0f9c6e61f9b17d62ef57afa
Megha Bose <megh.bose@gmail.com>
2022-11-18 19:39:49 +0530

    added notebooks

--- agents/Filtering.py
+++
@@ -1,8 +0,0 @@
-from headers import * 
-
-# Created filtering agent for extracting relevant data from the datasets
-
-class Agent:
-
-    def __init__(self):
-        pass
--- agents/Recommender.py
+++
@@ -1,8 +0,0 @@
-from headers import *
-
-# Creates Recommender agent that sends recommendations to user
-
-class Agent:
-    
-    def __init__(self):
-        pass
